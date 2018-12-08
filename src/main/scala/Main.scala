3fce392feb2b63a434059af99ef901281a5c01f4
SandroDamilano <damilano.sandro@gmail.com>
2018-12-08 16:08:47 -0300

    Made any element able to be searched for

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,7 +3,12 @@ import java.io.File
 object Main extends App{
   val originalFile = new File(args(0))
   val diffFile = new File(args(1))
-  val elementId = "make-everything-ok-button"
+  var elementId = "make-everything-ok-button"
+
+  if(!args(2).isEmpty){
+    elementId = args(2)
+  }
+
   val path = new ElementFinder(originalFile, elementId).findIn(diffFile)
   println(path)
 }
